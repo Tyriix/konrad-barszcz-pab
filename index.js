f80e8449364a3853d80667a7b297c6914ccb7b6e
Konrad Barszcz <konrad.barszcz@microsoft.wsei.edu.pl>
2022-03-20 21:10:11 +0100

    Working operations on numbers (final)

--- index.js
+++ index.js
@@ -2,15 +2,29 @@
 exports.__esModule = true;
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) {
-    var num1 = req.query.num1;
-    var num2 = req.query.num2;
-    var operation = {
-        add: function (num1, num2) { return num1 + num2; },
-        sub: function (num1, num2) { return num1 - num2; },
-        div: function (num1, num2) { return num1 / num2; },
-        mul: function (num1, num2) { return num1 * num2; }
-    };
-    res.send(operation, num1, num2);
+app.get('/:num1/:num2/:operation', function (req, res) {
+    //let operations = {
+    //    add: (num1, num2) => res.send(num1 + num2),
+    //    sub:  (num1, num2) => res.send(num1 - num2),
+    //    div: (num1, num2) => res.send(num1 / num2),
+    //    mul: (num1, num2) => res.send(num1 * num2),
+    //}
+    var num1 = +req.params.num1;
+    var num2 = +req.params.num2;
+    var operation = req.params.operation;
+    switch (operation) {
+        case 'add':
+            res.send("Tw\u00F3j wynik to ".concat(num1 + num2, "."));
+            break;
+        case 'sub':
+            res.send("Tw\u00F3j wynik to ".concat(num1 - num2, "."));
+            break;
+        case 'div':
+            res.send("Tw\u00F3j wynik to ".concat(num1 / num2, "."));
+            break;
+        case 'mul':
+            res.send("Tw\u00F3j wynik to ".concat(num1 * num2, "."));
+            break;
+    }
 });
 app.listen(3000);
--- index.ts
+++ index.ts
@@ -2,27 +2,34 @@ import { Server } from "http"
 
 const express = require('express')  
 const app = express()
-app.get('/', function (req, res) { 
-    let num1 = req.query.num1;
-    let num2 = req.query.num2;
-    let operation = req.query.operation;
-    let operations = {
-        add: (num1, num2) => num1 + num2,
-        sub:  (num1, num2) => num1 - num2,
-        div: (num1, num2) => num1 / num2,
-        mul: (num1, num2) => num1 * num2,
-    }
+
+app.get('/:operation/:num1/:num2', function (req, res) { 
+    
+    //let operations = {
+    //    add: (num1, num2) => res.send(num1 + num2),
+    //    sub:  (num1, num2) => res.send(num1 - num2),
+    //    div: (num1, num2) => res.send(num1 / num2),
+    //    mul: (num1, num2) => res.send(num1 * num2),
+    //}
+    
+    let num1 = +req.params.num1;
+    let num2 = +req.params.num2;
+    let operation = req.params.operation;
+
     switch(operation){
-        case 'dodaj':
-            operations.add;
-        case 'odejmij':
-            operations.div;
-        case 'pomnoz':
-            operations.mul;
-        case 'dziel':
-            operations.div;
+        case 'add':
+            res.send(`Twój wynik to ${num1 + num2}.`);
+            break;
+        case 'sub':
+            res.send(`Twój wynik to ${num1 - num2}.`);
+            break;
+        case 'div':
+            res.send(`Twój wynik to ${num1 / num2}.`);
+            break;
+        case 'mul':
+            res.send(`Twój wynik to ${num1 * num2}.`);
+            break;
     }
-    let wynik;
-    res.send(wynik.toString());
+    
 })
 app.listen(3000)
