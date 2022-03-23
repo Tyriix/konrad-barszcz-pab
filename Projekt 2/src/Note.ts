c2ad64ffbac1a2985b2ae5f422c0acc0af1bedb4
Konrad Barszcz <konrad.barszcz@microsoft.wsei.edu.pl>
2022-03-23 19:54:32 +0100

    Fixed PUT, Added /notes Endpoint , Added Tag Api

--- Projekt 2/src/Note.ts
+++ Projekt 2/src/Note.ts
@@ -1,11 +1,12 @@
+import Tag from '../src/Tag'
 class Note {
     public title: string;
     public content: string;
     public createDate?: string;
-    public tags?: string[];
+    public tags?: Tag[];
     public id?: number;
   
-    constructor(title: string, content: string, createDate?: string, tags?: string[], id?: number)
+    constructor(title: string, content: string, createDate?: string, tags?: Tag[], id?: number)
     {
       this.title = title;
       this.content = content;
--- Projekt 2/src/index.ts
+++ Projekt 2/src/index.ts
@@ -1,22 +1,43 @@
 import {Request, Response} from 'express'
 import Note from '../src/Note'
+import Tag from '../src/Tag'
 const express = require('express')  
 const app = express()
 
 app.use(express.json())
+import fs from 'fs';
+import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from 'constants'
 
 const notes: Note[] = [];
+const tags: Tag[] = [];
 
-const validate = (data: Note) =>{
+const validateNote = (data: Note) =>{
   if(data.title == null || data.title == "") return false
   if(data.content == null || data.content == "") return false
   return true;
 }
 
+//const validateTag = (data: Tag) =>{
+//  if(data.name == null || data.name == "") return false
+//  return true;
+//}
+
+//function validateTag(val: string)
+//{
+// for(var i=0; i<tags.length; i++){
+//   if(tags[i] == val){
+//      return true;
+//    }
+//  }
+//}
+
+// -----NOTES-----
+//ADD NOTE
 app.post('/note', (req: Request, res: Response)=>{
   const note: Note = req.body;
-  if(!validate(note))
+  if(!validateNote(note))
   {
+
     res.status(400).send("Nie podałeś tytułu lub treści.");
   }
   else
@@ -27,6 +48,7 @@ app.post('/note', (req: Request, res: Response)=>{
   }
 })
 
+//GET ONE NOTE
 app.get('/note/:id', (req: Request, res: Response) =>
 {
   const id = +req.params.id;
@@ -41,6 +63,7 @@ app.get('/note/:id', (req: Request, res: Response) =>
   }
 })
 
+//UPDATE NOTE
 app.put('/note/:id', (req: Request, res: Response) =>
 {
   const updatedNote: Note = req.body;
@@ -50,10 +73,11 @@ app.put('/note/:id', (req: Request, res: Response) =>
   {
     res.status(404).send("Ta notatka nie istnieje.");
   }
-  notes[id] = updatedNote;
+  notes[notes.indexOf(currentNote!)] = updatedNote;
   res.status(204).send(updatedNote);
 })
 
+//DELETE NOTE
 app.delete('/note/:id', (req: Request, res: Response) =>
 {
   const id = +req.params.id;
@@ -68,6 +92,80 @@ app.delete('/note/:id', (req: Request, res: Response) =>
   }
 })
 
+//GET LIST OF NOTES
+app.get('/notes', (req: Request, res: Response) =>
+{
+  if(notes.length === 0)
+  {
+    res.status(400).send(`Brak notatek.`)
+  }
+  res.status(200).send(notes);
+})
+
+
+// -----TAGS-----
+//GET ONE TAG
+app.get('/tag/:id', (req:Request, res:Response) =>
+{
+  const id = +req.params.id;
+  const tag = tags.find(x => x.id === id);
+  if(tags == undefined)
+  {
+    res.status(404).send("Ten tag nie istnieje.");
+  }
+  else
+  {
+  res.status(200).send(tag);
+  }
+})
+
+//ADD TAG
+app.post('/tag', (req: Request, res: Response)=>{
+  const tag: Tag = req.body;
+  tag.id = new Date().valueOf();
+  tags.push(tag)
+  res.status(201).send(tag)
+})
+
+//UPDATE TAG
+app.put('/tag/:id', (req: Request, res: Response) =>
+{
+  const updatedTag: Tag = req.body;
+  const id = +req.params.id;
+  let currentTag = tags.find(x => x.id === id)
+  if(currentTag == undefined)
+  {
+    res.status(404).send("Ten tag nie istnieje.");
+  }
+  tags[tags.indexOf(currentTag!)] = updatedTag;
+  res.status(204).send(updatedTag);
+})
+
+//DELETE TAG
+app.delete('/tag/:id', (req: Request, res: Response) =>
+{
+  const id = +req.params.id;
+  let tag = tags.find(x => x.id === id)
+  if(tag == undefined)
+  {
+    res.status(400).send(`Nie znaleziono tagu.`);
+  }
+  else{
+  tags.splice(tags.findIndex(x => x.id === (id)), 1)
+  res.status(204).send(`Usunięto notatkę.`);
+  }
+})
+
+//GET LIST OF TAGS
+app.get('/tags', (req: Request, res: Response) =>
+{
+  if(tags.length === 0)
+  {
+    res.status(400).send(`Brak tagów.`)
+  }
+  res.status(200).send(tags);
+})
+
 
 app.get('/', function (req: Request, res: Response) {
   //const jsonNote = JSON.stringify(note)
@@ -78,5 +176,4 @@ app.post('/', function (req: Request, res: Response) {
   //const title = req.body.title 
   res.status(200).send('POST Hello World')
 })
-
 app.listen(3000)
